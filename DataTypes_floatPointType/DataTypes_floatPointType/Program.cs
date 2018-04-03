a521e5966156f7b74c8cad9005bc16605dcc411f
Mazen AZKAHE <ENGMAZENN93@Outlook.com>
2018-04-02 21:26:29 -0700

    float

---
+++ DataTypes_floatPointType/DataTypes_floatPointType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_floatPointType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+            Console.WriteLine("float Min is : " + flMin + "\nfloat Max is : " + flMax);
+            float flVal = 1f / 3;
+            double dlVal = 1d / 3;
+            decimal dcVal = 1m / 3;
+            Console.WriteLine(  "float : " + flVal+"\n"
+                               + "double : " + dlVal + "\n"
+                               + "decimal : " + dcVal);
+            //Console.ReadKey();
+        }
+    }
+}
